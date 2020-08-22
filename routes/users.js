ee77eadab7ebe4c7b98f8bc73a3a6233a452e39e
Darshan Pandit <darshan.pandit@hotmail.com>
2020-08-22 23:24:26 +0400

    added user list web api

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  //res.send('respond with a resource');
+  let xUsers = [
+    { id: 1, name: "darshan" },
+    { id: 2, name: "rangana" },
+    { id: 3, name: "sanchita" },
+    { id: 4, name: "misbah" }
+  ];
+
+  res.json(xUsers);
 });
 
 module.exports = router;
