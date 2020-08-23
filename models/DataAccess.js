e8520a74e2fb23a75b988ba4f50783c2b92b19f9
Darshan Pandit <darshan.pandit@hotmail.com>
2020-08-23 12:53:17 +0400

    added sqlite3 db connect

---
+++ models/DataAccess.js
@@ -0,0 +1,16 @@
+const sqlite3 = require('sqlite3').verbose();
+let dbFilePath = process.env.SQLITE3_DB_FILE_PATH;
+
+function DAO() {
+  console.log('Connecting to database file path:', dbFilePath);
+  this.db = new sqlite3.Database(dbFilePath, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log('Connected to Database:', dbFilePath);
+    }
+  });
+}
+
+exports.DAO = DAO;
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,18 @@
+const DataAccess = require('../models/DataAccess');
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  //res.send('respond with a resource');
-  let xUsers = [
-    { id: 1, name: "darshan" },
-    { id: 2, name: "rangana" },
-    { id: 3, name: "sanchita" },
-    { id: 4, name: "misbah" }
-  ];
-
-  res.json(xUsers);
+  let xUsers;
+  console.log(process.env.SQLITE3_DB_FILE_PATH);
+  var dao1 = new DataAccess.DAO();
+  dao1.db.serialize(() => {
+    dao1.db.all("Select id1, name1 From Users1", (err, rows) => {
+      xUsers = rows;
+      res.json(xUsers);
+    });
+  });
 });
 
 module.exports = router;
